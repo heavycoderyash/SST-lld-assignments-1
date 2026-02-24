6d743419c463d1fa1182f7146d4c2cc7811c410f
Yash Solanki <yashisyash13112004@gmail.com>
2026-02-24 14:03:59 +0530

    Delete Ex6 directory

--- Ex6/EmailSender.java
+++
@@ -1,9 +0,0 @@
-public class EmailSender extends NotificationSender {
-    public EmailSender(AuditLog audit) { super(audit); }
-
-    @Override
-    public void send(Notification n) {
-        System.out.println("EMAIL -> to=" + n.email + " subject=" + n.subject + " body=" + n.body);
-        audit.add("email sent");
-    }
-}
